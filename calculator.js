c8e4f26e7f88cc8e7f9e354e561a0c1b8adb0cb7
Abha Mam <apm6283@psu.edu>
2024-07-21 01:57:23 -0400

    adding operator screen
    NOTE: issue with operator after calculate is called

--- calculator.js
+++ calculator.js
@@ -2,8 +2,8 @@
 
 var display = document.getElementById("disply");
 var display2 = document.getElementById("stodis");
+var display3 = document.getElementById("opScreen");
 
-var screen2_dis =""
 
 var a = 0;
 var b = 0;
@@ -12,31 +12,68 @@ var mode = "display";
 
 function appendToDisplay(input){
     
-    display.value += input;
+    //issue with inputing operator after = sign, display2 is outputing "answer=answer" upon pressing operator
+    
     str = display.value;
+    opp = display3.value;
 
-    if (str.length >= 11){
-        display.value = str.substring(0,11)
+    if (opp == "=" && (input == "+" || input == "-" || input == "*" ||input == "/")){
+        display2.value = str;
+        display3.value = input;        
+    }
+
+    if (input == "+"){
+        display3.value = "+";
+        display2.value += opp;
+        display2.value += str;
+        display.value = "";
+    }
+    else if (input == "-"){
+        display3.value = "-";
+        display2.value += opp;
+        display2.value += str;
+        display.value = "";
+    }
+    else if(input == "*"){
+        display3.value = "*";
+        display2.value += opp;
+        display2.value += str;
+        display.value = "";
+    }
+    else if(input == "/"){
+        display3.value = "/";
+        display2.value += opp;
+        display2.value += str;
+        display.value = "";
+    }
+    else{
+        display.value += input;
+    }
+
+    if (str.length >= 9){
+        display.value = str.substring(0,9)
     }
 }
 
 function clearDisplay(){
     display.value = "";
     display2.value = "";
+    display3.value = "";
 }
 
 function calculate(){
-    screen2_dis = display.value;
-    screen2_dis += "="
-    display2.value = screen2_dis;
+    display2.value += display3.value;
+    display2.value += display.value;
 
     try {
-        display.value = eval(disply.value);
+        display.value = eval(display2.value);
+
+        display3.value = "="
 
         str = display.value;
 
-        if (str.length >= 11){
-            display.value = str.substring(0,11)
+        if (str.length >= 10){
+            display.value = str.substring(0,9)
         }
     } catch (error) {
         display.value = "Error"     
